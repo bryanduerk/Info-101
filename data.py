1dbcb82cec3e3f26c7c92f253540228a7bc12364
bryanduerk <bryan.duerk@gmail.com>
2021-12-05 11:19:28 -0500

    Added some examples for aggregation and loops

--- data.py
+++ data.py
@@ -1,1 +1,38 @@
-# Basic file for repo
+from os import X_OK
+import pandas as pd
+
+class PowerLiftData:
+        
+    def ___init___(self):
+        self.data = []
+
+    def load(self):
+        self.data = pd.read_csv(r'data/usa_sbd_data_2020-10-16.csv')
+        self.npDataArray = self.data.to_numpy()
+
+
+    # This section uses Pandas, which simplifies statistics on tabular data
+    def ages(self):
+        print(self.data['Age'].mean())
+        self.data['AgeRank'] = pd.qcut(self.data['Age'], q = 5, labels = False)
+        groupedByAge = self.data.groupby(['AgeRank'])
+        ageStats  = groupedByAge['Age'].agg(['min', 'median', 'mean', 'max'])
+        print(ageStats)
+        print(groupedByAge['Squat1Kg'].mean())
+
+    def print(self):
+        print(self.data.head())
+        # print(list(self.data.columns))
+        
+    # This section uses a standard numerical python array and iterates
+    def printNames(self, firstName):
+        for row in self.npDataArray:
+            if (row[1].startswith(firstName)):
+                print(row)
+        
+
+runner = PowerLiftData()
+runner.load()
+runner.printNames('Joe')
+#runner.ages()
+#runner.print()
